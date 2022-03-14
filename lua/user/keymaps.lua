2d4626276b0cdac40cd8d725b791a90ab6ca1d98
Morgan Jensen <morganjensen2@mail.weber.edu>
2022-03-14 15:02:04 -0600

    added lsp support

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,38 +1,51 @@
-local opts = { noremap = true, silent = true } 
+local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
 
--- shorten function name
+-- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
--- set space as leader
+--Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Modes
--- normal = "n"
--- insert = "i"
--- visual = "v"
--- visual block = "x"
--- term = "t"
--- command = "c"
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
 
+-- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts) -- Ctrl + h = Move Left
-keymap("n", "<C-j>", "<C-w>j", opts) -- Ctrl + j = Move Down
-keymap("n", "<C-k>", "<C-w>k", opts) -- Ctrl + k = Move Up
-keymap("n", "<C-l>", "<C-w>l", opts) -- Ctrl + l = Move Right
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- File navigation
-keymap("n", "<leader>e", ":Lex 20<cr>", opts) -- opens Lexplore with size 20
-
+keymap("n", "<leader>e", ":Lex 20<cr>", opts) -- opens lexplore with size 20
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
@@ -40,10 +53,12 @@ keymap("v", ">", ">gv", opts)
 -- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
-
--- keep what you have in clipboard when pasting over something
--- instead of grabbing what you replaced
-keymap("v", "p", '"_dP', opts)
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,45 +3,45 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/wbthomason/packer.vim",
-        install_path,
-    }
-    print "Installing packer close and reopen Neovim..."
-    vim.cmd [[packadd packer.nvim]]
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
 end
 
--- Autocommand that reloads neovim when you save the plugins.lua file
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd [[
-    augroup pakcer_user_config
-        autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerSync
-    augroup end
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
 ]]
 
--- Use protected call so we don't error out on first use
+-- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-    return
+  return
 end
 
--- Have packer use popup window
+-- Have packer use a popup window
 packer.init {
-    display = {
-        open_fn = function()
-            return require("packer.util").float { border = "rounded" }
-        end,
-    },
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
 }
 
--- Install Plugins
+-- Install your plugins here
 return packer.startup(function(use)
-    
-    -- boilerplate
+
+  -- boilerplate
     use "wbthomason/packer.nvim"    -- Have packer manage itself
     use "nvim-lua/popup.nvim"       -- an implementation of Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim"     -- useful lua functions used by lots of plugins
@@ -60,9 +60,19 @@ return packer.startup(function(use)
     use "L3MON4D3/LuaSnip"              -- snippet engine
     use "rafamadriz/friendly-snippets"  -- a bunch of snippets to use
 
+    -- LSP 
+    use "neovim/nvim-lspconfig"             -- enable LSP
+    use "williamboman/nvim-lsp-installer"   -- simple ls installer
+    use "hrsh7th/cmp-nvim-lsp"
 
-    -- automatically set up config after cloning packer.vim
-    if PACKER_BOOTSTRAP then
-        require("packer").sync()
-    end
+    -- telescope
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
